57a364dc0b5459c3ee5cfc6c5969a5343d56f66b
Martin <mradvenis@gmail.com>
2016-08-14 17:53:33 +0000

    Added unread notification intent. Notifications could be read one by one now.

--- index.js
+++ index.js
@@ -12,48 +12,116 @@ const ODATA_URL = `https://${DC_NAME}.sapjam.com/api/v1/OData/`;
 const configure = require('./server/requestBuilder')(ODATA_URL, JAM_TOKEN);
 const searchRequestHandler = require('./server/searchHandler');
 
-app.use(morgan('combined'));
-app.use(bodyParser.urlencoded({extended: false}));
+var contextState = {};
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 app.use(express.static('static'));
+app.use(morgan('combined'));
 
 app.listen(port, function() {
-  console.log(`Server started at port ${port}`);
+    console.log(`Server started at port ${port}`);
 });
 
 app.post('/voice', function(req, res) {
     var params = req.body.result.parameters;
+    var contexts = req.body.result.contexts;
+
     switch (req.body.result.action) {
+        case 'unreadNotificationCount':
+            request(configure(`Notifications_UnreadCount`), (error, response, body) => {
+                var count = body.d;
+                res.json({
+                    "speech": "This is the unread notification count",
+                    "data": [{
+                        text: 'There are ' + count + ' unread notifications.',
+                        uri: null,
+                        title: null,
+                        date: null,
+                        eventType: 'unreadNotificationCount'
+                    }]
+                });
+            });
+            break;
+
+
+
         case 'getNotifications':
-            var endPointParams = ['$expand=Sender'];
-            if (!params.generic_quantity){
+            var endPointParams = ['$expand=Sender,ObjectReference'];
+            if (!params.generic_quantity) {
                 endPointParams.push(`$top=${params.number || 5}`);
             }
 
             request(configure(`Notifications?` + endPointParams.join('&')), (error, response, body) => {
-              var notifications = body.d.results;
-              if (params.sender) {
-                  notifications = notifications.filter(notification => (
-                      notification.Sender.FirstName === params.sender || notification.Sender.LastName === params.sender || notification.Sender.FullName === params.sender
-                      ));
-              }
-              notifications = notifications.map(notification => {
-                  return {
-                      text: notification.Message,
-                      uri: notification.WebURL,
-                      title: notification.Description,
-                      date: notification.CreatedAt,
-                      eventType: notification.EventType
-                  };
-              });
-              if (notifications.length) {
-                  res.json({"speech":"These are the notifications", "data": notifications});
-              } else {
-                  res.json({"speech": 'You have no notifications' + (params.sender ? ` from ${params.sender}.` : '.'), "data": []});
-              }
-              });
+                var notifications = body.d.results;
 
+                if (params.sender) {
+                    notifications = notifications.filter(notification => (
+                        notification.Sender.FirstName === params.sender || notification.Sender.LastName === params.sender || notification.Sender.FullName === params.sender
+                    ));
+                }
+
+                contextState['notifications'] = notifications;
+                contextState['notificationCount'] = 0;
+
+                if (notifications.length) {
+                    res.json({
+                        "speech": "Here are your unread notifications",
+                        "data": notifications.map(notification => {
+                            return {
+                                text: notification.Message,
+                                uri: notification.ObjectReference.WebURL,
+                                title: notification.Description,
+                                date: notification.CreatedAt,
+                                eventType: notification.EventType
+                            };
+                        })
+                    });
+                }
+                else {
+                    res.json({
+                        "speech": 'You have no unread notifications' + (params.sender ? ` from ${params.sender}.` : '.'),
+                        "data": []
+                    });
+                }
+            });
             break;
+
+
+
+
+        case 'readNotifications':
+            const notifications = contextState['notifications'];
+            const current = notifications[0];
+            contextState['currentNotification'] = current;
+            
+            var speech = 'You have no more notifications left to read.';
+            
+            if (current) {
+                speech = `${current.Description} ` + (current.Message ? current.Message : '');
+            }
+            
+            if (contextState['notificationCount'] == 0) {
+                speech = 'Here\'s the first notification - ' + speech;
+            } else if (notifications.length == 1) {
+                speech = 'Here\'s the last one - ' + speech;
+            } else {
+                speech = 'This is the next one - ' + speech;
+            }
+
+            res.json({
+                speech: speech,
+                data: notifications
+            });
+            
+            notifications.shift();
+            contextState['notificationCount'] = contextState['notificationCount'] + 1;
+            break;
+
+
+
         case 'whoAmI':
             request(configure('Self'), (error, response, body) => {
                 const self = body.d.results;
@@ -74,7 +142,7 @@ app.post('/voice', function(req, res) {
             });
             break;
         case 'search':
-            request(configure(`Search?Query='${params.query}'`), searchRequestHandler(configure, params, res));
+            request(configure(`Search?Query='${params.query}'&$expand=ObjectReference`), searchRequestHandler(configure, params, res));
             break;
         default:
             res.status(400).end();
--- server/searchHandler.js
+++ server/searchHandler.js
@@ -7,42 +7,27 @@ const SEARCH_RESOLVE_LIMIT = 3;
 module.exports = function searchRequestHandler(requestBuilder, params, res) {
 
     return (error, response, body) => {
-        resolveSearchRequests(body.d.results, params, res);
+        error ? res.status(500).end() : resolveSearchRequests(body.d.results, params, res);
     };
 
     function resolveSearchRequests(searchResults, params, res) {
         const searchCount = searchResults.length;
         const hasManyResults = searchCount > SEARCH_RESOLVE_LIMIT;
-
-        parallel(searchResults.slice(0, SEARCH_RESOLVE_LIMIT).map(resolveSearchRequest), (error, results) => {
-            if (!error) {
-                res.json({
-                    speech: `I have found ${searchCount} search results for "${params.query}".` + (hasManyResults ? ' Here are the first ${SEARCH_RESOLVE_LIMIT}.' : ''),
-                    data: results.map(standardize)
-                });
-            }
-            else {
-                res.status(500).end();
-            }
+        
+        res.json({
+            speech: `I have found ${searchCount} search results for "${params.query}".` + (hasManyResults ? ' Here are the first ${SEARCH_RESOLVE_LIMIT}.' : ''),
+            data: searchResults.map(standardize)
         });
     }
-
-    function resolveSearchRequest(searchResult) {
-        return callback => {
-            request(requestBuilder(`SearchResults('${searchResult.Id}')?$expand=ObjectReference`), (error, response, body) => {
-                callback(body.d.error, body.d.results);
-            });
-        };
-    }
-
-    function standardize(resolvedSearchResult) {
-        const item = resolvedSearchResult.ObjectReference;
+    
+    function standardize(searchResult) {
+        const item = searchResult.ObjectReference;
         return {
             title: item.Title,
             text: '',
             uri: item.WebURL,
-            date: resolvedSearchResult.CreatedAt,
+            date: searchResult.CreatedAt,
             eventType: item.Type
-        };
+        }
     }
 };
